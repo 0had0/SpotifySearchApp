82316ddd6e8a1e3200133244087a56861459580d
0had0 <hadihoussainy75@gmail.com>
2020-01-18 17:53:18 +0200

    Updates

---
+++ precache-manifest.7a2205246392211758f3314989637b7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161aeeb4897a925c18519977ad262494",
+    "url": "/SpotifySearchApp/index.html"
+  },
+  {
+    "revision": "6387917b7101fd85dfb8",
+    "url": "/SpotifySearchApp/static/css/main.61c6834d.chunk.css"
+  },
+  {
+    "revision": "1108951f5a53c92140de",
+    "url": "/SpotifySearchApp/static/js/2.db9ec9ea.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/SpotifySearchApp/static/js/2.db9ec9ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6387917b7101fd85dfb8",
+    "url": "/SpotifySearchApp/static/js/main.f1ac574a.chunk.js"
+  },
+  {
+    "revision": "ad4404adfe105d584fef",
+    "url": "/SpotifySearchApp/static/js/runtime-main.10433a52.js"
+  }
+]);
